54f3888e2e4fc8d931fee915388147f23e5a0d28
Hendardi Yoga Pratama <141380863+Hen2721@users.noreply.github.com>
2023-09-28 23:22:34 +0700

    menambahkan layout sign up

---
+++ app/src/main/java/com/wahidabd/synrgynguli/activity_sign_up_psw.kt
@@ -0,0 +1,11 @@
+package com.wahidabd.synrgynguli
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_sign_up_psw : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up_psw)
+    }
+}
